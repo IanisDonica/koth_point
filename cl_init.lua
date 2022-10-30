5822b058d867c059f0f5362d5611704023b07305
IanisDonica <64953588+IanisDonica@users.noreply.github.com>
2022-10-30 18:53:43 +0200

    Add files via upload

---
+++ cl_init.lua
@@ -0,0 +1,165 @@
+AddCSLuaFile("shared.lua")
+include("shared.lua")
+
+function ENT:Draw()
+    self:DrawModel()
+end
+
+hook.Add("PostDrawTranslucentRenderables", 'AOUUUUGH', function()
+
+    render.SetColorMaterial()
+    net.Receive("Broadcastcappoint", function()
+    
+        position_point = net.ReadVector()
+
+    end)
+
+    render.DrawSphere( position_point, cap_radius, 30, 30, Color( 0, 175, 175, 100 ) )
+end)
+
+surface.CreateFont("DisplayFont", {
+    font = "Bahnschrift",
+	extended = false,
+	size = 40,
+	weight = 500,
+	blursize = 0,
+	scanlines = 0,
+	antialias = true,
+	underline = false,
+	italic = false,
+	strikeout = false,
+	symbol = false,
+	rotary = false,
+	shadow = false,
+	additive = false,
+	outline = false,
+})
+
+surface.CreateFont("BottomFont", {
+    font = "Bahnschrift",
+	extended = false,
+	size = 20,
+	weight = 500,
+	blursize = 0,
+	scanlines = 0,
+	antialias = true,
+	underline = false,
+	italic = false,
+	strikeout = false,
+	symbol = false,
+	rotary = false,
+	shadow = false,
+	additive = false,
+	outline = false,
+})
+
+w = ScrW()
+x = 0
+y = 0
+x1 = 0
+y1 = 0
+x2 = 0
+y2 = 0
+cap = 0 
+x3 = 0
+y4 = 0 
+x4 = 0
+y5 = -100
+x5 = -100
+bottom = 0 
+cap_read = 0
+bottom_text_x = -100
+bottom_text_y = -100
+bottom_inner = 0
+changecolor = Color(86, 87, 86)
+cap_radius = 0
+position_point = Vector(0, 0,0)
+
+hook.Add("HUDPaint", "dfsfdsfsd", function()
+    draw.RoundedBox(10, w/2 - 550, 25, x2, y2, Color(41, 40, 40))
+    draw.RoundedBox(2, w/2 - 502, 98, x4, y4, Color(41, 40, 40))
+    draw.RoundedBox(2, w/2 - 500, 100, x3, y, Color(86, 87, 86))
+    draw.RoundedBox(2, w/2 - 500, 100, x, y, Color(66, 145, 255))
+    draw.RoundedBox(0, w/2 - 500, 100, x1, y1, Color(255, 0, 0))
+    surface.SetFont( "DisplayFont" )
+	surface.SetTextColor( 255, 255, 255 )
+	surface.SetTextPos( x5, y5 ) 
+	surface.DrawText( "You are on top of the hill" )
+    fw, fh = surface.GetTextSize("You are on top of the hill")
+    draw.RoundedBox(3, w/2 - 29, ScrH()-64, bottom, 58, Color(41, 40, 40))
+    draw.RoundedBox(3, w/2 - 25, ScrH()-60, bottom_inner, 50, changecolor)
+    surface.SetFont( "DisplayFont" )
+	surface.SetTextPos( bottom_text_x, bottom_text_y )
+    if cap_read > 0 then  surface.DrawText( "+"..cap_read) else	surface.DrawText( cap_read ) end
+    if cap_read > 0 then  fw1, fh1 = surface.GetTextSize("+"..cap_read) else fw1, fh1 = surface.GetTextSize(cap_read) end
+end)
+
+net.Receive("HudDraw1", function()
+    net.Receive("Getcap", function()
+        cap = net.ReadInt(16)
+    end)
+    x2 = 1100
+    y2 = 75
+    x3 = 1000
+    y = 20
+    y4 = 24
+    x4 = 1004
+    x5 = w/2 - fw /2
+    y5 = 40
+    if cap > 0 then 
+        x = 1000 * (cap / 1000)
+        y = 20
+    end
+    if cap < 0 then 
+        x = 0
+        x1 = 1000 * (math.abs(cap) / 1000)
+        y1 = 20
+    end
+    bottom_text_x = w/2 - fw1/2
+    if cap > 0 then changecolor = Color(66, 145, 255) elseif cap < 0 then changecolor = Color(255, 0, 0) else changecolor = Color(86, 87, 86) end
+end)
+
+net.Receive("HudNoDraw1", function()
+    x = 0
+    y = 0
+    x1 = 0
+    x2 = 0
+    y4 = 0
+    x4 = 0
+    y5 = -100
+    x5 = -100
+    bottom_text_x = w/2 - fw1/2
+    if cap > 0 then changecolor = Color(66, 145, 255)  elseif cap < 0 then changecolor = Color(255, 0, 0) else changecolor = Color(86, 87, 86) end
+end)
+
+
+net.Receive("Entity Spawned", function()
+    bottom = 58
+    bottom_text_x = w/2 - fw1/2
+    bottom_text_y = ScrH() - 57
+    bottom_inner = 50
+    cap_radius = 300
+end)
+
+function ENT:OnRemove()
+    x = 0
+    y = 0
+    x1 = 0
+    x2 = 0
+    y4 = 0
+    x4 = 0
+    y5 = -100
+    x5 = -100
+    bottom = 0
+    bottom_text_x = -100
+    bottom_inner = 0
+    cap_radius = 0
+end
+
+net.Receive("cap_bottom", function()
+    cap_read = net.ReadInt(16)
+end)
+
+net.Receive("sendrad", function()
+    cap_radius = net.ReadInt(16)
+end)
---
+++ init.lua
@@ -0,0 +1,158 @@
+AddCSLuaFile("cl_init.lua")
+AddCSLuaFile("shared.lua")
+
+include("shared.lua")
+
+function ENT:Initialize()
+
+    self:SetModel("models/props_gameplay/cap_point_base.mdl")
+    self:PhysicsInit(SOLID_VPHYSICS)
+    self:SetMoveType(MOVETYPE_VPHYSICS)
+    self:SetSolid(SOLID_VPHYSICS)
+
+    local phys = self:GetPhysicsObject()
+
+    if phys:IsValid() then
+        phys:Wake()
+    end
+end
+
+util.AddNetworkString("HudDraw1")
+util.AddNetworkString("HudNoDraw1")
+util.AddNetworkString("Getcap")
+util.AddNetworkString("Entity Spawned")
+util.AddNetworkString("cap_bottom")
+util.AddNetworkString("sendrad")
+util.AddNetworkString("Broadcastcappoint")
+
+playerlist = {}
+tred = {}
+tblue = {}
+nnet = 0
+n = 0
+cap = 0 
+entity_spawned = 0
+cap_radius = 300
+position_ofpoint = Vector(0,0,0)
+
+function ENT:OnRemove()
+    cap = 0 
+    entity_spawned = 0
+end
+
+
+function Checkifonpoint(ply)
+    if x:Distance(ply:GetPos()) < cap_radius then
+        ply:ChatPrint("You cant change your teams while on the point")
+        return 0
+    end
+    return 1
+end
+
+hook.Add("PlayerSay", "textcheck2", function(ply, text)
+    if text == "!setteam red" then 
+        if Checkifonpoint(ply) == 1 then
+            playerlist[ply] = "red"
+            ply:Kill()
+            ply:Spawn()
+        end
+    elseif text == "!setteam blue" then 
+        if Checkifonpoint(ply) == 1 then
+            playerlist[ply] = "blue"
+            ply:Kill()
+            ply:Spawn()
+        end
+    end
+end)
+
+function ENT:SpawnFunction(ply,tr,ClassName) 
+    classname_entity = ClassName
+    if entity_spawned == 0 then
+        local SpawnPos = tr.HitPos + tr.HitNormal * 10
+        local SpawnAng = ply:EyeAngles()
+
+        local ent = ents.Create( ClassName )
+        ent:SetPos( SpawnPos )
+        ent:SetAngles( SpawnAng )
+        ent:Spawn()
+        ent:Activate()
+
+        net.Start("Entity Spawned")
+        net.Broadcast()
+        entity_spawned = 1
+    else
+        ply:ChatPrint("You can only have 1 Controll point at a time")
+    end
+end
+
+
+time = CurTime()
+
+function ENT:Think()
+    x = self:GetPos()
+    if time + n < CurTime()
+    then
+        for k, v in pairs(ents.GetAll()) do
+            if v:IsPlayer() 
+            then 
+                if x:Distance(v:GetPos()) < cap_radius
+                then
+                    net.Start("HudDraw1")
+                    net.Send(v)
+                    if notInside(tred, v) 
+                    then
+                        if notInside(tblue, v)
+                        then
+                            if playerlist[v] == "red" then
+                                tred[#tred + 1] = v
+                            elseif playerlist[v] == "blue" then
+                                tblue[#tblue + 1] = v
+                            end
+                        end
+                    end
+                end
+                if x:Distance(v:GetPos()) > cap_radius
+                then
+                    net.Start("HudNoDraw1")
+                        net.Send(v)
+                    if playerlist[v] == "red" then
+                        table.remove(tred, #tred)
+                    elseif playerlist[v] == "blue" then
+                        table.remove(tblue, #tblue)
+                    end
+                end
+            end
+        end
+        cap = cap + ( #tblue - #tred )
+        if cap > 1000 then cap = 1000 end
+        if cap < -1000 then cap = -1000 end
+        net.Start("Getcap")
+            net.WriteInt(cap, 16)
+        net.Broadcast()
+        net.Start("cap_bottom")
+            net.WriteInt( #tblue - #tred, 16)
+        net.Broadcast()
+        n = n + 0.1
+    end
+    net.Start("Broadcastcappoint")
+        net.WriteVector(self:GetPos())
+    net.Broadcast()
+end
+
+function notInside(t1, x)
+    for k, v in pairs(t1) do 
+        if t1[k] == x then return false end
+    end
+    return true
+end
+
+concommand.Add("remove_point", function()
+    ents.FindByClass(classname_entity)[1]:Remove()
+end)
+
+concommand.Add("set_capradios", function(use,le,ss, rad)
+    cap_radius = tonumber(rad)
+    net.Start("sendrad")
+        net.WriteInt(cap_radius, 16)
+    net.Broadcast()
+end)
